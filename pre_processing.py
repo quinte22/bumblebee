638aac9c485075a3577fdf05d8f780a74f970991
Lucas Fenaux <46611744+LucasFenaux@users.noreply.github.com>
2021-04-03 12:31:19 -0400

    added use of batch to tensor function

--- pre_processing.py
+++ pre_processing.py
@@ -3,8 +3,9 @@ import sys
 sys.path.insert(0, 'bumblebee/pno_ai/')
 from pno_ai.preprocess import PreprocessingPipeline
 from pno_ai.helpers import prepare_batches
+from pno_ai.train import batch_to_tensors
 import torch
-from torch.utils.data import TensorDataset, RandomSampler, Dataloader
+from torch.utils.data import TensorDataset, RandomSampler, DataLoader
 import numpy as np
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 pin_memory = (device.type == "cuda")
@@ -29,23 +30,26 @@ validation_sequences = pipeline.encoded_sequences['validation']
 batch_size = 16
 num_workers = 4
 
-training_batches, training_labels = prepare_batches(training_sequences, batch_size)
-validation_batches, validation_labels = prepare_batches(validation_sequences, batch_size)
+max_length = max((len(L)
+                  for L in (training_sequences + validation_sequences))) - 1
+
+training_batches = prepare_batches(training_sequences, batch_size)
+validation_batches = prepare_batches(validation_sequences, batch_size)
 
 print(np.shape(training_batches))
-print(np.shape(training_labels))
+print(np.shape(validation_batches))
 
-train_data = torch.tensor(training_batches)
-train_targets = torch.tensor(training_labels)
+train_data, train_targets, train_mask = batch_to_tensors(training_batches, 0,
+                                max_length)
 
-validation_data = torch.tensor(validation_batches)
-validation_targets = torch.tensor(validation_labels)
+validation_data, validation_targets, validation_mask = batch_to_tensors(validation_batches, 0,
+                                max_length)
 
 train_dataset = TensorDataset(train_data, train_targets)
 train_sampler = RandomSampler(train_data)
-train_loader = Dataloader(train_dataset, sampler=train_sampler, batch_size=batch_size, num_workers=num_workers,
+train_loader = DataLoader(train_dataset, sampler=train_sampler, batch_size=batch_size, num_workers=num_workers,
                           pin_memory=pin_memory)
 validation_dataset = TensorDataset(validation_data, validation_targets)
 validation_sampler = RandomSampler(validation_dataset)
-validation_loader = Dataloader(validation_dataset, sampler=validation_sampler, batch_size=batch_size,
+validation_loader = DataLoader(validation_dataset, sampler=validation_sampler, batch_size=batch_size,
                                num_workers=num_workers, pin_memory=pin_memory)
